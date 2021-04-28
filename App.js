5d878dd5511aa9cc6ae43d562a705c10b36b1d6c
angelkim88 <66551410+hoharon@users.noreply.github.com>
2021-04-28 21:50:48 +0900

    #1 complete container

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import Weather from "./Weather";
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "d2b8a2c4ea75891fea1fc4e5a1aabfef";
+
+function RecommendClothes(temp) {
+  if (temp >= 28) {
+    return 1;
+  } else if (23 <= temp && temp < 28) {
+    return 2;
+  } else if (20 <= temp && temp < 23) {
+    return 3;
+  } else if (17 <= temp && temp < 20) {
+    return 4;
+  } else if (12 <= temp && temp < 17) {
+    return 5;
+  } else if (9 <= temp && temp < 12) {
+    return 6;
+  } else if (5 <= temp && temp < 9) {
+    return 7;
+  } else {
+    return 8;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      temp,
+      condition: weather[0].main,
+    });
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestBackgroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("앱을 사용하기 위해 위치 허용을 해주세요");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading, temp, condition, choice } = this.state;
+    if (isLoading) {
+      return <Loading />;
+    } else {
+      if (temp > 0) {
+        return (
+          <Weather
+            temp={Math.round(temp)}
+            condition={condition}
+            choice={RecommendClothes(temp)}
+          />
+        );
+      } else {
+        null;
+      }
+    }
+  }
+}
---
+++ Weather.js
@@ -0,0 +1,210 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import LottieView from "lottie-react-native";
+
+const weatherOptions = {
+  Thunderstorm: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "천둥번개",
+    lottie: require("./weather/weather-storm.json"),
+  },
+  Drizzle: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "부슬비",
+    lottie: require("./weather/weather-partly-shower.json"),
+  },
+  Rain: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "비",
+    lottie: require("./weather/weather-partly-shower.json"),
+  },
+  Snow: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "눈",
+    lottie: require("./weather/weather-snow.json"),
+  },
+  Haze: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "안개",
+    lottie: require("./weather/weather-mist.json"),
+  },
+  Mist: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "안개",
+    lottie: require("./weather/weather-mist.json"),
+  },
+  Fog: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "안개",
+    lottie: require("./weather/weather-foggy.json"),
+  },
+  Clear: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "맑음",
+    lottie: require("./weather/weather-sunny.json"),
+  },
+  Clouds: {
+    gradient: ["#2980b9", "#6dd5fa", "#ffffff"],
+    title: "구름",
+    lottie: require("./weather/weather-windy.json"),
+  },
+};
+
+const clothesOption = {
+  1: {
+    top: "반팔 민소매",
+    bottom: "반바지 원피스",
+  },
+  2: {
+    top: "반팔 얇은 셔츠",
+    bottom: "반바지 면바지",
+  },
+  3: {
+    top: "얇은 가디건 긴팔",
+    bottom: "면바지 청바지",
+  },
+  4: {
+    top: "얇은 니트 맨투맨",
+    bottom: "가디건 청바지",
+  },
+  5: {
+    top: "자켓 가디건 야상",
+    bottom: "스타킹 청바지 면바지",
+  },
+  6: {
+    top: "자켓 트렌치코트 야상",
+    bottom: "니트 청바지 스타킹",
+  },
+  7: {
+    top: "코트 가죽자켓",
+    bottom: "히트텍 니트 레깅스",
+  },
+  8: {
+    top: "롱패딩 두꺼운 코드",
+    bottom: "목도리 기모제품",
+  },
+};
+
+export default function Weather({ temp, condition, choice }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.vacant}></View>
+      <LinearGradient
+        colors={weatherOptions[condition].gradient}
+        style={styles.upperContainer}
+      >
+        <View style={styles.halfContainer}>
+          <LottieView
+            style={{
+              width: 180,
+              height: 180,
+              marginLeft: 20,
+            }}
+            source={weatherOptions[condition].lottie}
+            autoPlay
+            loop
+          />
+          <View style={styles.halfContainer__columm}>
+            <Text style={styles.temp}>{temp}℃</Text>
+            <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+          </View>
+        </View>
+        <LottieView
+          style={{
+            width: 150,
+            height: 150,
+            marginLeft: -15,
+            marginTop: 26,
+          }}
+          source={require("./person/person-man.json")}
+          autoPlay
+          loop
+        />
+      </LinearGradient>
+      <View style={styles.halfContainer}>
+        <Text style={styles.subtitle}>What to wear?</Text>
+      </View>
+      <View style={styles.halfContainer__choice}>
+        <Text style={styles.subtitle}>{clothesOption[choice].top}</Text>
+        <Text style={styles.subtitle}>{clothesOption[choice].bottom}</Text>
+      </View>
+    </View>
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Mist",
+    "Haze",
+    "Fog",
+    "Clear",
+    "Clouds",
+  ]).isRequired,
+  choice: PropTypes.number.isRequired,
+};
+
+const styles = StyleSheet.create({
+  vacant: {
+    width: 20,
+    height: 100,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  upperContainer: {
+    width: 350,
+    height: 280,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    borderRadius: 20,
+  },
+  temp: {
+    fontSize: 42,
+    color: "white",
+  },
+  halfContainer: {
+    flexDirection: "row",
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  halfContainer__columm: {
+    marginTop: 30,
+    flexDirection: "column",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  halfContainer__choice: {
+    flex: 1,
+    marginTop: -120,
+    alignItems: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "100",
+  },
+  subtitle: {
+    color: "black",
+    fontSize: 35,
+    marginTop: 20,
+  },
+});
